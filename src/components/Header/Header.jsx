79b20ce31cd1ce32f5bb486e930c421386566704
Anna Motorina <scripa57@gmail.com>
2023-05-20 23:31:49 +0300

    Add fontAwesome icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import styles from './Header.module.css';
+// import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+// src="./../../../public/hare.png"
 
 const Header = () => {
   return (
     <header className={styles.header}>
+      <FontAwesomeIcon icon={faBars} className={styles.icon_bar} />
       <img
         className={`${styles.header} ${styles.logo}`}
         src="https://cdn.pixabay.com/photo/2016/12/27/13/10/logo-1933884__340.png"
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,36 @@
 import styles from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUserVneck,
+  faMessages,
+  faNewspaper,
+  faMusic,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul>
         <li>
+          <FontAwesomeIcon icon={faUserVneck} />
           <NavLink to="/profile">Profile</NavLink>
         </li>
         <li>
+          <FontAwesomeIcon icon={faMessages} />
           <NavLink to="/dialogs">Messages</NavLink>
         </li>
         <li>
+          <FontAwesomeIcon icon={faNewspaper} />
           <NavLink to="/news">News</NavLink>
         </li>
         <li>
+          <FontAwesomeIcon icon={faMusic} />
           <NavLink to="/music">Music</NavLink>
         </li>
         <li>
+          <FontAwesomeIcon icon={faGear} />
           <NavLink to="/settings">Settings</NavLink>
         </li>
       </ul>
